254fc894cdce8a97e9d1b84b8ef896f2b5c946bb
mary-garachu <garachu.muthoni@gmail.com>
2023-06-16 14:25:53 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 9; num1++)
+	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = num1 + 1; num2 < 9; num2++)
+		for (num2 = 0; num2 < 10; num2++)
 		{
 
 			putchar((num1 % 10) + '0');
@@ -20,7 +20,7 @@ int main(void)
 			if (num1 == 9 && num2 == 9)
 
 			putchar(',');
-			putchar(32);
+			putchar(' ');
 		}
 	}
 
