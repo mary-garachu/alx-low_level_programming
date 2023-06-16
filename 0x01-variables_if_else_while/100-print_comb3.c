cb23a04500790f39d3fe09bd374eae5c9180de63
mary-garachu <garachu.muthoni@gmail.com>
2023-06-16 14:27:50 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,9 +18,10 @@ int main(void)
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
 			if (num1 == 9 && num2 == 9)
+				continue;
 
 			putchar(',');
-			putchar(' ');
+			putchar(32);
 		}
 	}
 
