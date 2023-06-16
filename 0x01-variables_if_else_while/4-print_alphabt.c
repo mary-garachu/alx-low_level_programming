684b3bc3a9a68fd0fd041d31e7a644cded6d0989
mary-garachu <garachu.muthoni@gmail.com>
2023-06-16 08:26:30 -0700

    4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+/*
+ * File: 4-print_alphabt.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l != 'e' && l != 'q')
+			putchar(l);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
