0970f22bfc3a53474c62f3606df63942da52b70c
mary-garachu <garachu.muthoni@gmail.com>
2023-06-16 09:05:49 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+/*
+ * File: 7-print_tebahpla.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	char l = 'z';
+
+	while (l >= 'a';)
+		putchar(l);
+		l--;
+
+	putchar('\n');
+
+	return (0);
+}
