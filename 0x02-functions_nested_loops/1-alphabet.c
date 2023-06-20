39100f9082d134ee8895889cbef8475c2a69be43
mary-garachu <garachu.muthoni@gmail.com>
2023-06-20 05:49:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *main-prints the alphabet, in lowercase, followed by a new line.
+ *Author: Mary Garachu
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; a <= 'z'; a++)
+		_putchar(l);
+
+	_putchar('\n');
+}
