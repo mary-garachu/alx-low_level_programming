fc9b007674bd82527ccf2bb1a3b85752fd0a5a01
mary-garachu <garachu.muthoni@gmail.com>
2023-07-20 06:46:05 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,11 +13,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list arg;
 	unsigned int i;
+	char *str;
 
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
-		const char *str = va_arg(arg, const char *);
+		str = va_arg(arg, char *);
 
 		if (str != NULL)
 		{
