3428155899d13dbe3f6d0d3099564f6a498200bc
sonie <garachu.muthoni@gmail.com>
2023-10-03 07:35:24 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ascii_value in range(ord('a'), ord('z') + 1):
-    print(chr(ascii_value), end='')
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
