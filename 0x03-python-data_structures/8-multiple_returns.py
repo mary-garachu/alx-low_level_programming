2e50571aa89b72ac1162410768ab7ebb1f76eab8
sonie <garachu.muthoni@gmail.com>
2023-10-09 07:43:19 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    str_len, first_char = len(sentence), sentence[0]
-    return (str_len, first_char)
+    if len(sentence) == 0:
+        return 0, None
+    else:
+        return len(sentence), sentence[0]
